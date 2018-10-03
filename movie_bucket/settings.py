c548ac23d5973d04223e96c15413de3d0d243f05
Dar <dev910626@gmail.com>
2018-10-03 19:34:33 +0800

    change allowed host ip address

--- movie_bucket/settings.py
+++ movie_bucket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4dcrl-dnq#pk)rq$mp((7yu*agh3tio2i(-0k9v!-p&(@vozi6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.33.10']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
