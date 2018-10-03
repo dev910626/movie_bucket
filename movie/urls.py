8dafe7ccb7dd75e15910e588d268ac58e20d9043
Dar <dev910626@gmail.com>
2018-10-03 05:55:44 +0000

    add display for list and detail page

--- movie/urls.py
+++ movie/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from movie.views import ListPage
+from movie.views import *
 
 app_name = 'movie'
 
 urlpatterns = [
     path('', ListPage.as_view(), name='list'),
+    path('movie/<int:movie_id>/', DetailPage.as_view(), name='detail'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -2,26 +2,29 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
-
 from .models import *
 
+from .classes.movie import MovieClass as m_c
+from .classes.movie_detail import MovieDetailClass as md_c
+
 class ListPage(View):
-	TEMPLATE_NAME = 'list_page.html'
+	TEMPLATE_NAME = 'movie/list.html'
 
 	def get(self, request, *args, **kwargs):
-		movies = Movie.objects.filter(is_active=True)
+		template_vars = {
+			'movies': m_c.get_active_movies()
+		}
 
-		return render(self.request, self.TEMPLATE_NAME, {'movies': movies})
+		return render(self.request, self.TEMPLATE_NAME, template_vars)
 
 
 class DetailPage(View):
-	TEMPLATE_NAME = 'detail_page.html'
-
-	def __init__(self, movie_id):
-		self.movie = Movie.objects.filter(pk=movie_id)
+	TEMPLATE_NAME = 'movie/detail.html'
 
 	def get(self, request, *args, **kwargs):
-		movie_detail = MovieDetail.objects.filter(movie=self.movie)
+		template_vars = {
+			'movie_detail': md_c.get_movie_detail_by_movie_id(self.kwargs['movie_id'])
+		}
 
-		return render(self.request, self.TEMPLATE_NAME, {'movie_detail': movie_detail})
+		return render(self.request, self.TEMPLATE_NAME, template_vars)
 
